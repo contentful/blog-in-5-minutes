72c9fcc233d4cdd0c878c10f7536b6a8fab81a20
stefan judis <stefanjudis@gmail.com>
2017-05-18 14:58:51 +0200

    Improve config handling

--- plugins/contentful-client.js
+++ plugins/contentful-client.js
@@ -2,28 +2,32 @@ let ctfConfig
 
 try {
   ctfConfig = require('../.contentful')
-} catch (e) {
-  if (
-    !process.env.CTF_PERSON_ID ||
-    !process.env.CTF_BLOG_POST_TYPE_ID ||
-    !process.env.CTF_SPACE_ID ||
-    !process.env.CTF_CDA_ACCESS_TOKEN ||
-    !process.env.CTF_CMA_ACCESS_TOKEN
-  ) {
-    throw new Error(
-      'Please provide needed Contentful configs:\n' +
-      'There are two way to do so:\n' +
-      '- define a .contentful.js file (similar to .contentful.sample) in the root of this directory\n' +
-      '- define environment variables CTF_PERSON_ID, CTF_BLOG_POST_TYPE_ID, CTF_SPACE_ID, CTF_ACCESS_TOKEN'
-    )
-  }
+} catch (error) {
+  if (error.code === 'MODULE_NOT_FOUND') {
+    if (
+      !process.env.CTF_PERSON_ID ||
+      !process.env.CTF_BLOG_POST_TYPE_ID ||
+      !process.env.CTF_SPACE_ID ||
+      !process.env.CTF_CDA_ACCESS_TOKEN ||
+      !process.env.CTF_CMA_ACCESS_TOKEN
+    ) {
+      throw new Error(
+        'Please provide needed Contentful configs:\n' +
+        'There are two way to do so:\n' +
+        '- define a .contentful.js file (similar to .contentful.sample) in the root of this directory\n' +
+        '- define environment variables CTF_PERSON_ID, CTF_BLOG_POST_TYPE_ID, CTF_SPACE_ID, CTF_ACCESS_TOKEN'
+      )
+    }
 
-  ctfConfig = {
-    CTF_PERSON_ID: process.env.CTF_PERSON_ID,
-    CTF_BLOG_POST_TYPE_ID: process.env.CTF_BLOG_POST_TYPE_ID,
-    CTF_SPACE_ID: process.env.CTF_SPACE_ID,
-    CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN,
-    CTF_CMA_ACCESS_TOKEN: process.env.CTF_CMA_ACCESS_TOKEN
+    ctfConfig = {
+      CTF_PERSON_ID: process.env.CTF_PERSON_ID,
+      CTF_BLOG_POST_TYPE_ID: process.env.CTF_BLOG_POST_TYPE_ID,
+      CTF_SPACE_ID: process.env.CTF_SPACE_ID,
+      CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN,
+      CTF_CMA_ACCESS_TOKEN: process.env.CTF_CMA_ACCESS_TOKEN
+    }
+  } else {
+    throw error
   }
 }
 
