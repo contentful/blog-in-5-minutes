a1e1ee1cf4dbabe579efa8d0cd12e0ce5d4100d9
Benedikt Rötsch <benedikt@contentful.com>
2017-07-13 11:14:20 +0200

    fix(data-import): check for file access before cloning

--- bin/download-content-model.js
+++ bin/download-content-model.js
@@ -1,24 +1,35 @@
+const fs = require('fs')
 const path = require('path')
 
 const clone = require('git-clone')
 const rimraf = require('rimraf')
 
-const DATA_DIR = path.resolve(__dirname, '..', 'data')
+const REPO_ROOT = path.resolve(__dirname, '..')
+const DATA_DIR = path.resolve(REPO_ROOT, 'data')
 
-console.log('Cloning content model')
+console.log('Checking permissions to write files to the disk')
 
-rimraf(DATA_DIR, () => {
-  clone(
-    'git@github.com:contentful/content-models.git',
-    DATA_DIR,
-    (error) => {
-      if (error) {
-        console.log('An error happend during cloning')
-        console.log(error)
-        process.exit(1)
+fs.access(REPO_ROOT, fs.constants.W_OK, (err) => {
+  if (err) {
+    console.error(`Script is unable to write to project directory. Please check your users file permissions for ${REPO_ROOT}`)
+    process.exit(1)
+  }
+
+  console.log('Cloning content model')
+
+  rimraf(DATA_DIR, () => {
+    clone(
+      'git@github.com:contentful/content-models.git',
+      DATA_DIR,
+      (error) => {
+        if (error) {
+          console.log('An error happend during cloning')
+          console.log(error)
+          process.exit(1)
+        }
+
+        console.log('Cloned data to ./data')
       }
-
-      console.log('Cloned data to ./data')
-    }
-  )
+    )
+  })
 })
