e1069a5bb23815264677952e76806e1773b98b8b
Benedikt Rötsch <benedikt@contentful.com>
2017-07-13 11:02:44 +0200

    fix(data-import): clean up data dir before fresh data git clone

--- bin/download-content-model.js
+++ bin/download-content-model.js
@@ -1,16 +1,24 @@
-const clone = require('git-clone')
 const path = require('path')
 
-console.log('Cloning content model');
-clone(
-  'git@github.com:contentful/content-models.git',
-  path.resolve(__dirname, '..', 'data'),
-  (error) => {
-    if (error) {
-      console.log('An error happend during cloning')
-      console.log(error)
-    }
+const clone = require('git-clone')
+const rimraf = require('rimraf')
 
-    console.log('Cloned data to ./data')
-  }
-)
+const DATA_DIR = path.resolve(__dirname, '..', 'data')
+
+console.log('Cloning content model')
+
+rimraf(DATA_DIR, () => {
+  clone(
+    'git@github.com:contentful/content-models.git',
+    DATA_DIR,
+    (error) => {
+      if (error) {
+        console.log('An error happend during cloning')
+        console.log(error)
+        process.exit(1)
+      }
+
+      console.log('Cloned data to ./data')
+    }
+  )
+})
