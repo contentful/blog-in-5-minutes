e312bb29f7d3ebc7df08b49c0ed6100d60aec893
Benedikt Rötsch <benedikt@contentful.com>
2017-07-14 10:10:24 +0200

    feat(data-import): add ora spinners for proper appeal

--- bin/download-content-model.js
+++ bin/download-content-model.js
@@ -2,33 +2,40 @@ const fs = require('fs')
 const path = require('path')
 
 const clone = require('git-clone')
+const ora = require('ora')
 const rimraf = require('rimraf')
 
 const REPO_ROOT = path.resolve(__dirname, '..')
 const DATA_DIR = path.resolve(REPO_ROOT, 'data')
 
-console.log('Checking permissions to write files to the disk')
+const permissions = ora('Checking permissions to write files to the disk').start()
 
 fs.access(REPO_ROOT, fs.constants.W_OK, (err) => {
   if (err) {
-    console.error(`Script is unable to write to project directory. Please check your users file permissions for ${REPO_ROOT}`)
+    permissions.fail(`Script is unable to write to project directory. Please check your users file permissions for ${REPO_ROOT}`)
     process.exit(1)
   }
 
-  console.log('Cloning content model')
+  permissions.succeed()
 
-  rimraf(DATA_DIR, () => {
+  const cloning = ora('Cloning content model').start()
+
+  rimraf(DATA_DIR, (error) => {
+    if (error) {
+      cloning.fail('An error happend while removing existing content model')
+      process.exit(1)
+    }
     clone(
       'git@github.com:contentful/content-models.git',
       DATA_DIR,
       (error) => {
         if (error) {
-          console.log('An error happend during cloning')
+          cloning.fail('An error happend during cloning')
           console.log(error)
           process.exit(1)
         }
 
-        console.log('Cloned data to ./data')
+        cloning.succeed('Cloned data to ./data\n')
       }
     )
   })
